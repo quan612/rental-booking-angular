6bdc1ed33c8c457d335561c48f216a7fc6d08491
quan612 <quan612@yahoo.com>
2020-11-12 17:11:59 -0500

    fix config issue in nodejs

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
   DB_URI: process.env.DB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
-  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_K,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
